3b5dc7689644d1124eee209f31e5b4d033bbe618
Fan Yang <yangfcm16@gmail.com>
2020-01-19 01:22:23 +1030

    Add anagrams and its test cases

---
+++ Algo/Anagrams.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Algo
+
+/**
+Anagram
+Check to see if two provided strings are anagrams of each other.
+One string is an anagram of the other if it uses the same characters in the same quantity.
+Only conside characters, not spaces or punctuation.
+Conside capital letters to be the same as lower case
+e.g. anagrams('rail safety', 'fairy tales') --> true
+anagrams('RAIL! SAFETY!', 'fairy tales')  --> true
+angagrams('Hi, there', 'Bye, there') --> false
+ */
+{
+	public class Anagrams
+	{
+		static public bool Main(string str1, string str2)
+		{
+			string sortedStr1 = Alphabetize(CleanString(str1));
+			string sortedStr2 = Alphabetize(CleanString(str2));
+			return sortedStr1 == sortedStr2;
+		}
+
+		/**
+			Remove spaces and punctuations of a string and convert it to lowercase
+		*/
+		private static string CleanString(string str)
+		{
+			string pattern = @"\W";
+			string clearedStr = Regex.Replace(str, pattern, "");
+			return clearedStr.ToLower();
+		}
+
+		/**
+		 Sort a string on alphabetic order
+		 e.g. 'sad' -> 'ads'
+		 */
+		private static string Alphabetize(string str)
+		{
+			char[] arr = str.ToCharArray();
+			Array.Sort(arr);
+			Console.WriteLine(new string(arr));
+			return new string(arr);
+		}
+	}
+}
---
+++ Test/AnagramsTest.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algo;
+
+namespace Test
+{
+	[TestClass]
+	public class AnagramsTest
+	{
+		[TestMethod]
+		[DataRow("abcde", "debca")]
+		[DataRow("HELLO", "llohe")]
+		[DataRow("Whoa!    Hi!", "hi!whoa!")]
+		public void TestAnagramsReturnTrue(string str1, string str2)
+		{
+			var result = Anagrams.Main(str1, str2);
+			Assert.IsTrue(result, $"{str1} should be an anagram of {str2}");
+		}
+
+		[TestMethod]
+		[DataRow("One One", "Two Two")]
+		public void TestAnagramsReturnFalse(string str1, string str2)
+		{
+			var result = Anagrams.Main(str1, str2);
+			Assert.IsFalse(result, $"{str1} should NOT be an anagram of {str2}");
+		}
+	}
+}

c5ac758046d2aac60f7ce2be656972810d6a6059
Fan Yang <yangfcm16@gmail.com>
2020-11-22 21:57:21 +1030

    fix typo

--- Algo/Dynamic/MaxProfit.cs
+++ Algo/Dynamic/MaxProfit.cs
@@ -1,5 +1,12 @@
 using System.Linq;
 
+/**
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+ * e.g. [7, 1, 5, 3, 6, 4] => 7 ( (5-1) + (6-3) = 7 )
+ * @param {number[]} prices
+ * @return {number}
+ */
 namespace Algo.Dynamic
 {
   public class MaxProfit

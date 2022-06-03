3c5954cd812ec3cbfefd72a331731334d29e4806
Fan Yang <yangfcm16@gmail.com>
2022-06-04 00:17:35 +0930

    Fix sum lists.

--- Algo/Ds/LinkedList/SumLists.cs
+++ Algo/Ds/LinkedList/SumLists.cs
@@ -8,7 +8,7 @@ using System;
 *
 * Then suppose the digits are stored in forward order. Repeat the above problem
 * e.g. (6 -> 1 -> 7) + (2 -> 9 -> 5) => 617+295 = 912 => 9 -> 1 -> 2
-* Difficult: Do not convert to number. How to do that?
+* Do not convert it to number and consider two lists have different length.
 */
 namespace Algo.Ds.LinkedList
 {
@@ -73,5 +73,29 @@ namespace Algo.Ds.LinkedList
       }
       return list;
     }
+
+    static public MyLinkedList<int> Run(MyLinkedList<int> list1, MyLinkedList<int> list2)
+    {
+      var sumList = new MyLinkedList<int>();
+      int count1 = list1.Count;
+      int count2 = list2.Count;
+      int maxCount = count1 > count2 ? count1 : count2;
+      var node1 = list1.Head;
+      var node2 = list2.Head;
+
+      for (int i = 0; i < maxCount; i++) {
+          int digit1 = node1 == null ? 0 : node1.Data;
+          int digit2 = node2 == null ? 0 : node2.Data;
+          var lastDigit = sumList.FindLast() == null ? 0 : sumList.FindLast().Data;
+          sumList.RemoveLast();
+          int digitSum = digit1 + digit2 + lastDigit;
+          sumList.AddLast(digitSum % 10);
+          sumList.AddLast(digitSum / 10);
+
+          if(node1 != null) node1 = node1.Next;
+          if(node2 != null) node2 = node2.Next;
+        }
+        return sumList;
+      }
   }
 }
--- Test/Ds/LinkedList/SumListsTest.cs
+++ Test/Ds/LinkedList/SumListsTest.cs
@@ -15,12 +15,6 @@ namespace Test.Ds.LinkedList
     {
       list1 = new MyLinkedList<int>();
       list2 = new MyLinkedList<int>();
-      list1.AddLast(7);
-      list1.AddLast(1);
-      list1.AddLast(6);
-      list2.AddLast(5);
-      list2.AddLast(9);
-      list2.AddLast(2);
     }
 
     [TestCleanup]
@@ -31,22 +25,49 @@ namespace Test.Ds.LinkedList
     }
 
     [TestMethod]
-    public void TestSumRev()
+    public void TestSumLists1()
     {
-      var sumList = SumLists.SumRev(list1, list2);
+      list1.AddLast(7);
+      list1.AddLast(1);
+      list1.AddLast(6);
+      list2.AddLast(5);
+      list2.AddLast(9);
+      list2.AddLast(2);
+      var sumList = SumLists.Run(list1, list2);
       Assert.AreEqual(sumList.Find(0).Data, 2);
       Assert.AreEqual(sumList.Find(1).Data, 1);
       Assert.AreEqual(sumList.Find(2).Data, 9);
     }
 
     [TestMethod]
-    public void TestSumFwd()
+    public void TestSumLists2()
     {
-      var sumList = SumLists.SumFwd(list1, list2);
-      Assert.AreEqual(sumList.Find(0).Data, 1);
-      Assert.AreEqual(sumList.Find(1).Data, 3);
+      list1.AddLast(9);
+      list1.AddLast(9);
+      list1.AddLast(9);
+      list1.AddLast(9);
+      list2.AddLast(1);
+      var sumList = SumLists.Run(list1, list2);
+      Assert.AreEqual(sumList.Find(0).Data, 0);
+      Assert.AreEqual(sumList.Find(1).Data, 0);
       Assert.AreEqual(sumList.Find(2).Data, 0);
-      Assert.AreEqual(sumList.Find(3).Data, 8);
+      Assert.AreEqual(sumList.Find(3).Data, 0);
+      Assert.AreEqual(sumList.Find(4).Data, 1);
+    }
+
+    [TestMethod]
+    public void TestSumLists3()
+    {
+      list1.AddLast(9);
+      list1.AddLast(8);
+      list2.AddLast(9);
+      list2.AddLast(9);
+      list2.AddLast(9);
+      var sumList = SumLists.Run(list1, list2);
+      Assert.AreEqual(sumList.Find(0).Data, 8);
+      Assert.AreEqual(sumList.Find(1).Data, 8);
+      Assert.AreEqual(sumList.Find(2).Data, 0);
+      Assert.AreEqual(sumList.Find(3).Data, 1);
     }
   }
 }

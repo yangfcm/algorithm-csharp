3112306fb838b7504c03962f9bcac8a723f437e9
Fan Yang <yangfcm16@gmail.com>
2022-10-19 11:12:44 +1030

    Add problem and solution to MinimumAbsoluteDifference.

--- Algo/Greedy/MinimumAbsoluteDifference.cs
+++ Algo/Greedy/MinimumAbsoluteDifference.cs
@@ -1,14 +1,37 @@
+using System;
+
 namespace Algo.Greedy
 {
     /// <summary>
-    /// 
+    ///  Given an array of integers, find the minimum absolute difference between any two elements in the array.
     /// </summary>
     /// <example>
+    /// arr = [1 -3 71 68 17] -> 3 (|71-68|= 3)
     /// </example>
     /// <source>
+    /// https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array
     /// </source>
     public class MinimumAbsoluteDifference
     {
-        
+        /// <summary>
+        /// Sort and iterate through the array.
+        /// Calculate the absolute difference between the number and its next number and find out the minimum one.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns>The minimum absolute difference found.</returns>
+        public static int Solution(int[] arr)
+        {
+            int minDiff = Int32.MaxValue;
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int diff = Math.Abs(arr[i] - arr[i + 1]);
+                if(diff < minDiff)
+                {
+                    minDiff = diff;
+                }
+            }
+            return minDiff;
+        }
     }
 }
--- Test/Algo/Greedy/MinimumAbsoluteDifferenceTest.cs
+++ Test/Algo/Greedy/MinimumAbsoluteDifferenceTest.cs
@@ -3,8 +3,16 @@ using Algo.Greedy;
 
 namespace Test.Algo.Greedy
 {
+    [TestClass]
     public class MinimumAbsoluteDifferenceTest
     {
-        
+        [TestMethod]
+        [DataRow(new int[]{3, -7 ,0}, 3)]
+        [DataRow(new int[]{-59, -36, -13, 1, -53, -92, -2, -96, -54, 75}, 1)]
+        [DataRow(new int[]{1, -3, 71, 68, 17}, 3)]
+        public void TestSolution(int[] arr, int expected)
+        {
+            Assert.AreEqual(expected, MinimumAbsoluteDifference.Solution(arr));
+        }
     }
 }
